ed5badddb3b3d7181dfae5427adfe8baf8d6ed09
neamoth <neamoth15-8202@diu.edu.bd>
2025-01-29 17:52:16 +0600

    feature: Updated 9 fun fact on react and style them

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,32 @@ import { createRoot } from "react-dom/client";
 const root = createRoot(document.getElementById("root"));
 
 function CreateFunFact(){
+    const facts = [
+    "Just a library, but feels like a full framework. 😆",
+    "One state change = React re-renders everything! 🤯",
+    "useEffect() → Infinite re-renders → Dev panic. 🔄🔥",
+    "Class components? We don’t talk about them anymore. 😂",
+    "JSX: HTML in JS? JS in HTML? Both?! 🤔",
+    "useState(0), useEffect(), useMemo() → Brain overload. 🤯",
+    "Why isn’t my prop updating? → UseEffect dependency hell. 🔥",
+    'Debugging React: "console.log everywhere!" 🕵️‍♂️',
+    'Deploys app → "Works on my machine!" 😎',
+    ];
     return(
-        <div className="imageContainer">
-            <img className="image" src="/src/assets/react.svg" alt="React Logo" />
-        </div>
+        <main>
+            <div className="imageContainer">
+                <img className="image" src="/src/assets/react.svg" alt="React Logo" />
+            </div>
+            <div className="listContainer">
+                <ul>
+                    {facts.map((facts, index) => (
+                        <li key={index} className="list">
+                            {index+1}. {facts}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </main>
     );
 }
 
